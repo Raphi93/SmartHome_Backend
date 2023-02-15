433f9c1adbdaaf06ef96491abef68493fac3a07e
Raphael Hug <raphael.hug@student.ipso.ch>
2023-02-15 22:56:40 +0100

    Final 1.0

--- SmartHome_Backend_NoSQL/Program.cs
+++ SmartHome_Backend_NoSQL/Program.cs
@@ -1,8 +1,8 @@
-using SmartHome_Backend_NoSQL.Controllers;
 using SmartHome_Backend_NoSQL.Service;
 using SmartHome_Backend_NoSQL.Models;
+using Microsoft.OpenApi.Models;
 
-internal class Program
+public class Program
 {
     private static void Main(string[] args)
     {
@@ -10,28 +10,24 @@ internal class Program
 
         builder.Services.Configure<WeatherStationDataBaseSetting>(
                   builder.Configuration.GetSection("WeatherStationDatabase"));
-
         builder.Services.AddScoped<IWeatherstation, WeatherstationMongoDB>();
-
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "JWT", Version = "v1" });
+        });
         builder.Services.AddSwaggerGen();
-
         var app = builder.Build();
-
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
             app.UseSwaggerUI();
         }
-
         app.UseHttpsRedirection();
-
         app.UseAuthorization();
-
         app.MapControllers();
-
         app.Run();
     }
 }
